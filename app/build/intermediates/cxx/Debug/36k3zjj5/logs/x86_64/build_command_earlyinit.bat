09ab9dd6678c2b3903d08d1ea7943463ad988644
Inamullah Lakho <inamullah.lakho@trapezegroup.com>
2025-06-27 15:15:12 +0400

    Added more ABIs

---
+++ app/build/intermediates/cxx/Debug/36k3zjj5/logs/x86_64/build_command_earlyinit.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\inamullah.lakho\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\inamullah.lakho\\Downloads\\EarlyInitFullProject\\app\\.cxx\\Debug\\36k3zjj5\\x86_64" ^
+  earlyinit
